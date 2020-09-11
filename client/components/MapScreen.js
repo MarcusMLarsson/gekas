0a9de6c01556b5f4e7e9f9b741250ea6998651a8
Larsson <marcus.larsson@cgi.com>
2020-09-11 21:10:41 +0200

    add airbnbmap

--- client/components/MapScreen.js
+++ client/components/MapScreen.js
@@ -1,29 +1,58 @@
  
 import React, {useState} from 'react';  
 import {StyleSheet, Text, TextInput, Button, View,Image,Dimensions} from 'react-native';
+import MapView from 'react-native-maps';
+
+var mapStyle = [
+  {
+      "featureType": "administrative.country",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "visibility": "simplified"
+          },
+          {
+              "hue": "#ff0000"
+          }
+      ]
+  }
+];
+
+
 
  const MapScreen = props => {
    
  return (  
-        <View style={styles.container}>  
-       
-       <Text>Map </Text>
+  <View style={styles.container}>
+        <MapView
+         style={styles.mapStyle} 
+         region={{
+          latitude: 57.13358228,
+          longitude: 12.714334399999984,
+          latitudeDelta: 0.0026,
+          longitudeDelta: 0.0
+        }}
+        provider={MapView.PROVIDER_GOOLE}
+        customMapStyle={mapStyle}
+         />
+      </View>
          
-        </View>  
     );  
 }
 
 
-const styles = StyleSheet.create({  
-    container: {  
-        flex: 1,  
-         flexDirection: 'column',
-        justifyContent: 'center',  
-        alignItems: 'center'  
-    },  
-
-
-});  
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
 
 export default MapScreen;
 
